03543aee06c5001233c71f4bed5056a2c158b10f
Igor Gulyaev <Igor Gulyaev>
2016-12-10 01:52:47 +0200

    4 Footer regions added

--- sites/all/themes/lifemedical/templates/system/page.tpl.php
+++ sites/all/themes/lifemedical/templates/system/page.tpl.php
@@ -166,8 +166,12 @@
   </div>
 </div>
 
-<?php if (!empty($page['footer'])): ?>
-  <footer class="footer <?php print $container_class; ?>">
-    <?php print render($page['footer']); ?>
-  </footer>
-<?php endif; ?>
+<footer class="footer <?php print $container_class; ?>">
+  <?php print render($page['footer']); ?>
+  <div class="row">
+    <div class="col-sm-3"><?php print render($page['footer1']); ?></div>
+    <div class="col-sm-4"><?php print render($page['footer2']); ?></div>
+    <div class="col-sm-2"><?php print render($page['footer3']); ?></div>
+    <div class="col-sm-3"><?php print render($page['footer4']); ?></div>
+  </div>
+</footer>
