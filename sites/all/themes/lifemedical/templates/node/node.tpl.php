b759372d3b78c23e2f9cff82ade466dc02222248
Igor Gulyaev <Igor Gulyaev>
2016-12-15 03:00:48 +0200

    Submitted forat edited (fixes 4)

--- sites/all/themes/lifemedical/templates/node/node.tpl.php
+++ sites/all/themes/lifemedical/templates/node/node.tpl.php
@@ -91,7 +91,7 @@
     <span class="submitted">
       <?php print $user_picture; ?>
       <?php if ($submitted) { 
-		echo date( "d m Y",$node->created);  
+		echo date( "d M Y",$node->created);  
 		} ?>
     </span>
     <?php endif; ?>
